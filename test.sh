f32b5335fecc27cd14db5b6d3bf9332e3f94aae5
Joe <joe.draper@gmail.com>
2021-04-15 11:26:38 -0700

    update

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 echo "Test project for configuring various Jenkins automated builds".
 echo "VERSION: 1.0.7"
+echo "REGION: ${REGION}"
 echo "SYSTEM_ID: ${SYSTEM_ID}"
 echo "IAM_LAMBDA_ROLE: ${IAM_LAMBDA_ROLE}"
 echo "DEPLOYMENT_BUCKET: ${DEPLOYMENT_BUCKET}"
