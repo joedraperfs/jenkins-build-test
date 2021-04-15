9a7ae3c3d14f4f47d47f2d5a350846a4bf9accc4
Joe <joe.draper@gmail.com>
2021-04-15 10:52:34 -0700

    Added jenkins pipeline script for testing how env vars are set

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 
 echo "Test project for configuring various Jenkins automated builds".
 echo "VERSION: 1.0.7"
+echo "SYSTEM_ID: ${SYSTEM_ID}"
+echo "IAM_LAMBDA_ROLE: ${IAM_LAMBDA_ROLE}"
+echo "DEPLOYMENT_BUCKET: ${DEPLOYMENT_BUCKET}"
